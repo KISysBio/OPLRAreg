6c42c4b4092f50dd2198c08049dfc1b480de7a83
Jon Cardoso <jonathan.car.silva@gmail.com>
2020-10-22 11:46:15 -0300

    Change version to 0.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
         "OPLRAreg is a regression technique based on mathematical programming that splits "
         "data into separate regions and identifies independent linear equations for each region."
     ),
-    version="0.2.1",
+    version="0.2.2",
     py_modules=["oplrareg"],
     author="Jonathan Cardoso Silva",
     author_email="jonathan.car.silva@gmail.com",
